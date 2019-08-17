a1978c6bfe94fbd88c4b3ac9256a797973527714
tudorgabriel24 <tudorgabriel1398@gmail.com>
2019-08-17 21:01:12 +0300

    add input-component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,15 @@ import React, { Component, Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import NotFound from '../NotFound/NotFound';
+import ComplexTextInput from '../shared/ComplexTextInput/ComplexTextInput';
+
 export default class Main extends Component {
   render() {
     return (
       <Fragment>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/input" component={ComplexTextInput}/>
           <Route component={NotFound} />
         </Switch>
       </Fragment>
