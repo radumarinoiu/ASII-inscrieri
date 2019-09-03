41c63bf980ada6b6b11393b7cdda58382d46e55a
Inga <ingaa.trifan@gmail.com>
2019-09-03 21:28:26 +0300

    new admin login component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 export default class NotFound extends Component {
   render() {
-    return <div>404-not found</div>;
+    return <div>
+       404 not found
+    </div>;
   }
 }
---
+++ src/components/login/AdminLogin.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import styles from './AdminLogin.module.scss'
+
+export default class AdminLogin extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: ''
+          }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        if (this.state.email == "admin@asii.ro" && this.state.password == "asii2019"){
+            alert('succes')
+        } else alert("try again")
+    }
+    handleChange (event) {
+        this.setState({ [event.target.name]: event.target.value} )
+      }
+    render() {
+        
+        return (
+            <div>
+                <div className={styles.container}>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Email
+                            <input 
+                                type="email" 
+                                placeholder="youareawsome@gmail.com" 
+                                name = "email"
+                                onChange={event => this.handleChange(event)}
+                                required>
+                            </input>
+                        </label>
+                        <label>Password
+                            <input 
+                                type='password' 
+                                placeholder="*********" 
+                                name='password' 
+                                minLength='8' 
+                                maxLength='20' 
+                                onChange={event => this.handleChange(event)} 
+                                required>
+                            </input>
+                        </label>
+                        <input type = 'submit'className ={styles.btn}></input>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
