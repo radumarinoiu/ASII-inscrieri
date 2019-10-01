2cbf5068ca86c52d2423aaf5d4b9201937c773fd
Gliga Dumitru <gliga_dumitru@yahoo.com>
2019-10-01 17:16:27 +0300

    correct desgin

--- src/components/ErrorSubmit/ErrorSubmit.jsx
+++ src/components/ErrorSubmit/ErrorSubmit.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import classnames from "classnames";
 import CustomButton from "../shared/CustomButton/CustomButton";
 
-export default class NotFound extends Component {
+export default class ErrorSubmit extends Component {
   handleClick = _ => {
     this.props.history.push("/");
   };
@@ -10,7 +10,16 @@ export default class NotFound extends Component {
     return (
       <Fragment>
         {this.props && (
-          <div className="main">
+          <div className="main normalView">
+            <a
+              className="asiiLogoLink"
+              title="ASII Page"
+              href="https://asii.ro/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img src="https://asii.ro/images/logo.png" alt="asii" />
+            </a>
             <div className="row">
               <div className="header-image col s12 m12 l12">
                 <img
@@ -31,29 +40,49 @@ export default class NotFound extends Component {
               <div className="row center">
                 <div className="register-form col s12 m8 offset-m2 l8 offset-l2 xl6 offset-xl3 center">
                   <div
-                    className={classnames("data-container col s12 m12 l12", {
-                      "step-one animated fadeIn": true
-                    })}
+                    className={classnames(
+                      "data-container col s12 m8 offset-m2 ",
+                      {
+                        "step-one finalStep": true
+                      }
+                    )}
                   >
-                    <div className="container-body center col s12 m6 offset-m3 l6 offset-l3">
-                      <div className="errorSubmitContainer col 12">
-                        <img
-                          src={
-                            process.env.PUBLIC_URL +
-                            "/assets/images/error-submit.png"
-                          }
-                          alt=""
-                        />
+                    <div className="container-body col s12">
+                      <div className="row">
+                        <div className="check-image-container col s12 m12 l12">
+                          <img
+                            src={
+                              process.env.PUBLIC_URL +
+                              "/assets/images/CheckError.png"
+                            }
+                            className="checkImage"
+                            alt=""
+                          />
+                        </div>
+                        <div className="col s12 m12">
+                          <img
+                            className="asii-logo-right"
+                            src={
+                              process.env.PUBLIC_URL +
+                              "/assets/images/error-submit.png"
+                            }
+                            alt=""
+                          />
+                        </div>
                       </div>
-                      <h3 className="header">EROARE!</h3>
-                      <h3 className="header small-text">
-                        A aparut o eroare la trimiterea informatiilor!
-                      </h3>
+                      <div className="row">
+                        <div className="success-text">
+                          <h2>eroare!</h2>
+                          <p>
+                            A aparut o eroare la trimiterea informatiilor.
+                          </p>
+                        </div>
+                      </div>
                       <div className="row">
                         <div className="container-buttons step0 col s12 m12 l12 xl12">
                           <CustomButton
                             type="back"
-                            title="Prima pagină"
+                            title="Incearca din nou"
                             disable={false}
                             handleClick={this.handleClick}
                           />
